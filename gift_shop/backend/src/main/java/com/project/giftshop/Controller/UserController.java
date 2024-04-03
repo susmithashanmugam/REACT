57869d9273ce8b9b81cd70d75d0075f933e21aaa
SUSMITHA <susmithashan34@gmail.com>
2024-04-03 12:47:07 +0530

    .

--- gift_shop/backend/src/main/java/com/project/giftshop/Controller/UserController.java
+++ gift_shop/backend/src/main/java/com/project/giftshop/Controller/UserController.java
@@ -3,42 +3,46 @@ package com.project.giftshop.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.project.giftshop.model.User;
 import com.project.giftshop.service.UserService;
-
+import com.project.giftshop.model.User;
 
 @RestController
-@RequestMapping("api/v1/user")
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @PostMapping("/save")
-    public ResponseEntity<?>saveUser(@RequestBody User user)
-    {
+    public ResponseEntity<?> saveUser(@RequestBody User user) {
         try {
             userService.saveUser(user);
-            return new ResponseEntity<>("User registered successfully",HttpStatus.ACCEPTED);
+            return new ResponseEntity<>("User registered successfully!", HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<>("Something went wrong",HttpStatus.EXPECTATION_FAILED);
+            return new ResponseEntity<>("Something went wrong!", HttpStatus.EXPECTATION_FAILED);
         }
     }
-
 
     @GetMapping("/get")
-    public ResponseEntity<?> getUserByEmail(@RequestParam String email)
-    {
+    public ResponseEntity<?> getUserByEmail(@RequestParam String email) {
         try {
-            return new ResponseEntity<>(userService.getUserByEmail(email),HttpStatus.OK);
-
+            return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>("Something went wrong",HttpStatus.OK);
-
+            return new ResponseEntity<>("Something went wrong", HttpStatus.EXPECTATION_FAILED);
         }
     }
+
     @GetMapping("/getAll")
     public ResponseEntity<?> getAllUser() {
         try {
@@ -47,57 +51,31 @@ public class UserController {
             return new ResponseEntity<>("Something went wrong", HttpStatus.EXPECTATION_FAILED);
         }
     }
-//    @PutMapping("/update/{email}")
-//    public ResponseEntity<?> updateUser(@PathVariable String email, @RequestBody User user) {
-//        try {
-//            return new ResponseEntity<>(userService.updateUser(email, user), HttpStatus.OK);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>("Something went wrong", HttpStatus.EXPECTATION_FAILED);
-//        }
-//    }
 
-    @PutMapping("/users/{email}")
-    public ResponseEntity<User> updateUser(@PathVariable String email, @RequestBody User updateUser) {
-        User updatedUser = userService.updateUser(email, updateUser);
-        return updatedUser != null ? ResponseEntity.ok(updatedUser) : ResponseEntity.notFound().build();
+    @PutMapping("/update/{email}")
+    public ResponseEntity<?> updateUser(@PathVariable String email, @RequestBody User user) {
+        try {
+            return new ResponseEntity<>(userService.updateUser(email, user), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Something went wrong", HttpStatus.EXPECTATION_FAILED);
+        }
     }
-//
-//    @DeleteMapping("/delete/{email}")
-//    public ResponseEntity<?> deleteUser(@PathVariable String email) {
-//        try {
-//            return new ResponseEntity<>(userService.deleteUser(email), HttpStatus.OK);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>("Something went wrong", HttpStatus.EXPECTATION_FAILED);
-//        }
-//    }
 
-//   @DeleteMapping("/users/{id}")
-//   public ResponseEntity<Void> deleteUser(@PathVariable String id)
-//   {
-//       boolean deleted = userService.deleteUser(id);
-//       return  deleted ? ResponseEntity.noContent().build();
-//       ResponseEntity.notFound().build();
-//   }
-
-
-@DeleteMapping("/delete/{email}")
-public ResponseEntity<Void> deleteUser(@PathVariable String email) {
-    boolean deleted = Boolean.parseBoolean(userService.deleteUser(email));
-    if (deleted) {
-        return ResponseEntity.noContent().build();
-    } else {
-        return ResponseEntity.notFound().build();
+    @DeleteMapping("/delete/{email}")
+    public ResponseEntity<?> deleteUser(@PathVariable String email) {
+        try {
+            return new ResponseEntity<>(userService.deleteUser(email), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Something went wrong", HttpStatus.EXPECTATION_FAILED);
+        }
     }
-}
 
-
-//    @PatchMapping("/patch/{email}")
+//    @PatchMapping("/patchUpdate/{email}")
 //    public ResponseEntity<?> updateUserBy(@PathVariable String email, @RequestBody User user) {
 //        try {
 //            return new ResponseEntity<>(userService.updateUserBy(email, user), HttpStatus.OK);
 //        } catch (Exception e) {
 //            return new ResponseEntity<>("Something went wrong", HttpStatus.EXPECTATION_FAILED);
 //        }
-//
 //    }
 }
--- gift_shop/backend/src/main/java/com/project/giftshop/service/UserService.java
+++ gift_shop/backend/src/main/java/com/project/giftshop/service/UserService.java
@@ -1,73 +1,51 @@
 package com.project.giftshop.service;
-
 import java.util.List;
 import java.util.Optional;
 
-import jakarta.persistence.Id;
+import com.project.giftshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import  com.project.giftshop.model.User;
 
-import com.project.giftshop.model.User;
-import com.project.giftshop.repository.UserRepository;
+import jakarta.transaction.Transactional;
 
 @Service
+@Transactional
 public class UserService {
 
+
     @Autowired
-     UserRepository userRepository;
+    private UserRepository userRepository;
 
     public void saveUser(User user) {
-       Optional<User> userExists=userRepository.findByEmail(user.getEmail());
-       if(userExists.isPresent())
-        return;
+        Optional<User> userExists = userRepository.findByEmail(user.getEmail());
+        if (userExists.isPresent())
+            return;
         userRepository.save(user);
     }
 
     public User getUserByEmail(String email) {
-        boolean userExists=userRepository.existsByEmail(email);
-        if(userExists)
+        boolean userExists = userRepository.existsByEmail(email);
+        if (userExists) {
             return userRepository.findByEmail(email).get();
-
-        return null;
+        }
+        return new User();
     }
 
     public List<User> getAllUser() {
         return userRepository.findAll();
     }
 
-//    public User updateUser(String email, User user) {
-//        Optional<User> userExists = userRepository.findByEmail(email);
-//        if (userExists.isPresent()) {
-//            User existingUser = userExists.get();
-//            existingUser.setId(user.getId());
-////            existingUser.setEmail(user.getEmail());
-//            existingUser.setName(user.getName());
-//            existingUser.setPassword(user.getPassword());
-//            return userRepository.save(existingUser);
-//        }
-
-//        return new User();
-//    }
-
     public User updateUser(String email, User user) {
         Optional<User> userExists = userRepository.findByEmail(email);
         if (userExists.isPresent()) {
-            User existingUser = userExists.get();;
+            User existingUser = userExists.get();
             existingUser.setName(user.getName());
-            existingUser.setEmail(user.getEmail());
-            // Update other fields as needed
+            existingUser.setPassword(user.getPassword());
             return userRepository.save(existingUser);
         }
         return new User();
     }
-//    public boolean deleteUser(String email) {
-//        if (userRepository.existsById(email)) {
-//            userRepository.deleteById(email);
-//            return true;
-//        }
-//        return false;
-//      }
-//}
 
     public String deleteUser(String email) {
         boolean userExists = userRepository.existsByEmail(email);
@@ -77,19 +55,13 @@ public class UserService {
         }
         return "Record not found with email id " + email;
     }
-//    public User updateUserBy(String email, User user) {
-//        Optional<User> userExists = userRepository.findByEmail(email);
-//        if (userExists.isPresent()) {
-//            User existingUser = userExists.get();
-//
-//                existingUser.setName(user.getName());
-//
-//
-//                existingUser.setPassword(user.getPassword());
-//
-//            return userRepository.save(existingUser);
-//        }
-//        return new User();
-//    }
 
+    public User updateUserBy(String email, User user) {
+        Optional<User> userExists = userRepository.findByEmail(email);
+        return userExists.map(existingUser -> {
+            Optional.ofNullable(user.getName()).ifPresent(existingUser::setName);
+            Optional.ofNullable(user.getPassword()).ifPresent(existingUser::setPassword);
+            return userRepository.save(existingUser);
+        }).orElse(new User());
+    }
 }
