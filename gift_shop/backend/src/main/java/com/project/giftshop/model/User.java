4ef1009f33bd6f602ef14ab80af753091434e4c5
SUSMITHA <susmithashan34@gmail.com>
2024-04-12 11:39:55 +0530

    .

--- gift_shop/backend/src/main/java/com/project/giftshop/model/User.java
+++ gift_shop/backend/src/main/java/com/project/giftshop/model/User.java
@@ -1,58 +1,87 @@
 package com.project.giftshop.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
-    private String name;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
     private String email;
-    private String password;
-
+    private String pw;
+    private String pw1;
+    private String journal;
+    @OneToOne(fetch=FetchType.EAGER,cascade=CascadeType.ALL)
+    @JoinColumn(name="id",referencedColumnName = "id")
+    private UserProfile userProfile;
+//    @OneToMany(mappedBy="user",cascade=CascadeType.ALL)
+//    private List<Book> books;
     public User() {
     }
-
-    public User(String id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
+    public User(String username, String email, String pw, String pw1,String journal,Long id,UserProfile userProfile) {
+        this.username = username;
         this.email = email;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
+        this.pw = pw;
+        this.pw1 = pw1;
+        this.journal = journal;
         this.id = id;
+        this.userProfile=userProfile;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
+    public UserProfile getUserProfile() {
+        return userProfile;
     }
-
+    public void setUserProfile(UserProfile userProfile) {
+        this.userProfile = userProfile;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public String getPassword() {
-        return password;
+    public String getPw() {
+        return pw;
     }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setPw(String pw) {
+        this.pw = pw;
     }
+    public String getPw1() {
+        return pw1;
+    }
+    public void setPw1(String pw1) {
+        this.pw1 = pw1;
+    }
+    public String getJournal() {
+        return journal;
+    }
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+//    public Long getId() {
+//        return id;
+//    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+
 }
--- gift_shop/backend/src/main/java/com/project/giftshop/service/UserService.java
+++ gift_shop/backend/src/main/java/com/project/giftshop/service/UserService.java
@@ -1,67 +1,98 @@
 package com.project.giftshop.service;
-import java.util.List;
+
+
 import java.util.Optional;
 
-import com.project.giftshop.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Service;
-import  com.project.giftshop.model.User;
+
+
+import com.project.giftshop.model.User;
+import com.project.giftshop.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 
 @Service
-@Transactional
 public class UserService {
-
 
     @Autowired
     private UserRepository userRepository;
 
     public void saveUser(User user) {
-        Optional<User> userExists = userRepository.findByEmail(user.getEmail());
-        if (userExists.isPresent())
+        Optional<User> userExists=userRepository.findByEmail(user.getEmail());
+        if(userExists.isPresent())
             return;
         userRepository.save(user);
     }
 
     public User getUserByEmail(String email) {
-        boolean userExists = userRepository.existsByEmail(email);
-        if (userExists) {
+        boolean userExists=userRepository.existsByEmail(email);
+        if(userExists)
             return userRepository.findByEmail(email).get();
-        }
-        return new User();
+
+        return null;
     }
 
-    public List<User> getAllUser() {
-        return userRepository.findAll();
-    }
-
-    public User updateUser(String email, User user) {
-        Optional<User> userExists = userRepository.findByEmail(email);
-        if (userExists.isPresent()) {
-            User existingUser = userExists.get();
-            existingUser.setName(user.getName());
-            existingUser.setPassword(user.getPassword());
+    public User updateUser(String email,User user)
+    {
+        Optional<User>userExists=userRepository.findByEmail(email);
+        if(userExists.isPresent())
+        {
+            User existingUser=userExists.get();
+            existingUser.setUsername(user.getUsername());
+            existingUser.setPw(user.getPw());
+            existingUser.setPw1(user.getPw1());
+            existingUser.setJournal(user.getJournal());
             return userRepository.save(existingUser);
+
+
         }
         return new User();
     }
+    @Transactional
+    public String deleteUser(String email)
+    {
 
-    public String deleteUser(String email) {
-        boolean userExists = userRepository.existsByEmail(email);
-        if (userExists) {
+        boolean userExists=userRepository.existsByEmail(email);
+        if(userExists)
+        {
             userRepository.deleteByEmail(email);
             return "User deleted successfully!";
+
         }
-        return "Record not found with email id " + email;
+        return "Record not found with email id "+email;
     }
 
-    public User updateUserBy(String email, User user) {
-        Optional<User> userExists = userRepository.findByEmail(email);
-        return userExists.map(existingUser -> {
-            Optional.ofNullable(user.getName()).ifPresent(existingUser::setName);
-            Optional.ofNullable(user.getPassword()).ifPresent(existingUser::setPassword);
+    public User updateUserBy(String email,User user)
+    {
+        Optional<User> userExists=userRepository.findByEmail(email);
+        return userExists.map(existingUser->{
+            Optional.ofNullable(user.getUsername()).ifPresent(existingUser::setUsername);
+            Optional.ofNullable(user.getPw()).ifPresent(existingUser::setPw);
+            Optional.ofNullable(user.getPw1()).ifPresent(existingUser::setPw1);
+            Optional.ofNullable(user.getJournal()).ifPresent(existingUser::setJournal);
             return userRepository.save(existingUser);
-        }).orElse(new User());
+        }).orElse(null);
+
+
     }
+    public Optional<User> getUserById(Long id)
+    {
+        return userRepository.findById(id);
+
+
+    }
+    public Page<User> getAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+
+
+
+
 }
